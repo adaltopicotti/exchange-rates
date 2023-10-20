dac41743759b87ad1a5ac7964c157996169fa53b
Adalto Picotti Junior <adaltocotti.jr@gmail.com>
2023-10-19 22:51:50 -0300

    Adicionando a listagem de flutuação de moedas e taxas

---
+++ ExchangeRates/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Adalto Picotti Junior on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatesFluctuationView()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
